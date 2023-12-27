e057fff83cd0d6e7915d8df10abb8d41164a8699
Some-Random-chip <154744016+Some-Random-chip@users.noreply.github.com>
2023-12-27 20:32:59 +0700

    Add files via upload
    yes.

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    
+    void Start()
+    {
+        // printin' hi
+        print("Hewwo, Wowld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
